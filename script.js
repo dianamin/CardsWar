5223d2e35eff21d403e407b29ec566595543023d
dianamin <97.diana@gmail.com>
2013-12-01 19:39:22 +0200

    It Begins.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
+
+
+var playerCard = document.getElementById('playerCard');
+playerCard.innerHTML = "random";
